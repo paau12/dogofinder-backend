1785ec37982eb4d6abefb3e5694946898c8c82c1
Yegua Celestial <yeguacelestial@gmail.com>
2020-10-21 00:02:00 -0500

    fix: Create db schema on flask run

--- api/__init__.py
+++ api/__init__.py
@@ -2,13 +2,18 @@ from flask import Flask
 
 from .extensions import db
 from .views import api
+from .models import *
 
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
-    db.init_app(app)
+
+    # Create db schema
+    with app.test_request_context():
+        db.init_app(app)
+        db.create_all()
 
     # Register blueprints
     app.register_blueprint(api)
--- api/models.py
+++ api/models.py
@@ -2,4 +2,5 @@ from .extensions import db
 
 
 class MyModel(db.Model):
+    # Create columns here
     pass
