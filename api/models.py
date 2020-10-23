c254d58a2860835a1aafcea5441210018a0da33b
Yegua Celestial <yeguacelestial@gmail.com>
2020-10-23 16:43:35 -0500

    Delete flask project

--- api/models.py
+++
@@ -1,6 +0,0 @@
-from .extensions import db
-
-
-class MyModel(db.Model):
-    # Create columns here
-    pass
--- api/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-api = Blueprint('api', __name__, url_prefix='/api')
-
-
-@api.route('/get_data')
-def get_data():
-    return {'key': 'value'}
