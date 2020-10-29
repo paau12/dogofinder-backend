97a8a2a68d8cb3267597c5b7ec979dc0514654ab
Diego Gil <diego_gilhalo98@hotmail.com>
2020-10-28 18:29:05 -0600

    Se Agregaron los modelos faltantes y las relaciones entre estos

--- api/models.py
+++ api/models.py
@@ -7,7 +7,7 @@
         - Crear modelo Reporte_perdido ---------- [x]
         - Crear modelo Reporte_avistado --------- [x]
         - Crear modelo Reporte_encontrado ------- [x]
-        - Relacionar modelos -------------------- [ ]
+        - Relacionar modelos -------------------- [x]
 '''
 
 from django.db import models
@@ -20,7 +20,7 @@ class Usuario(models.Model):
         primary_key=True
     )
 
-    correo_duenio = models.EmailField(max_length=100, unique=True)  # String
+    correo_duenio = models.EmailField(max_length=100, unique=True)  # Email
     nombre_duenio = models.CharField(max_length=100)  # String
     pais = models.CharField(max_length=100)  # String
     ciudad = models.CharField(max_length=100)  # String
@@ -28,7 +28,6 @@ class Usuario(models.Model):
     calle = models.CharField(max_length=100)  # String
     numero = models.CharField(max_length=100)  # String
 
-    #id_mascota = models.ForeignKey(Mascota, default=None, verbose_name='mascota', on_delete=models.SET_DEFAULT)  # Foreign Key
 
     class Meta:
         verbose_name_plural = "usuario"
@@ -62,7 +61,12 @@ class Mascota(models.Model):
 
     in_home = models.BooleanField(default=False)  # boolean
 
-    id_usuario = models.ForeignKey(Usuario, default=None, verbose_name="usuario", on_delete=models.CASCADE)  # Foreign Key
+    id_usuario = models.ForeignKey(
+            Usuario,
+            default=None,
+            verbose_name="usuario",
+            on_delete=models.CASCADE
+        )  # Foreign Key
 
     class Meta:
         verbose_name_plural = "mascota"
@@ -81,9 +85,12 @@ class Mascota_perdida(models.Model):
         primary_key=True
     )
 
-    id_mascota = models.ForeignKey(Mascota, default=None, verbose_name="mascota", on_delete=models.SET_DEFAULT)  # Foreign Key
-#    id_reporte_perdido = models.ForeignKey(Reporte_perdido, default=None, verbose_name="reporte_perido", on_delete=models.SET_DEFAULT)  # Foreign Key
-#    id_reporte_avistado = models.ForeignKey(Reporte_avistado, default=None, verbose_name="reporte_avistado", on_delete=models.SET_DEFAULT)  # Foreign Key
+    id_mascota = models.ForeignKey(
+            Mascota,
+            default=None,
+            verbose_name="mascota",
+            on_delete=models.SET_DEFAULT
+        )  # Foreign Key
 
     class Meta:
         verbose_name_plural = "mascota_perdida"
@@ -98,8 +105,12 @@ class Mascota_encontrada(models.Model):
         primary_key=True
     )
 
-    id_mascota = models.ForeignKey(Mascota, default=None, verbose_name="mascota", on_delete=models.SET_DEFAULT)  # Foreign Key
-#    id_reporte_encontrado = models.ForeignKey(Reporte_avistado, default=None, verbose_name="reporte_encontrado", on_delete=models.SET_DEFAULT)  # Foreign Key
+    id_mascota = models.ForeignKey(
+            Mascota,
+            default=None,
+            verbose_name="mascota",
+            on_delete=models.SET_DEFAULT
+        )  # Foreign Key
 
     class Meta:
         verbose_name_plural = "mascota_encontrada"
@@ -119,8 +130,18 @@ class Reporte(models.Model):
     fecha_reporte = models.DateField()  # Date
     descripcion_reporte = models.CharField(max_length=100)  # String
 
-    id_usuario = models.ForeignKey(Usuario, default=None, verbose_name="usuario", on_delete=models.CASCADE)  # Foreign Key
-    id_mascota = models.ForeignKey(Mascota, default=None, verbose_name="mascota", on_delete=models.CASCADE)  # Foreign Key
+    id_usuario = models.ForeignKey(
+            Usuario,
+            default=None,
+            verbose_name="usuario",
+            on_delete=models.CASCADE
+        )  # Foreign Key
+    id_mascota = models.ForeignKey(
+            Mascota,
+            default=None,
+            verbose_name="mascota",
+            on_delete=models.CASCADE
+        )  # Foreign Key
 
     class Meta:
         verbose_name_plural = "reporte"
@@ -146,7 +167,12 @@ class Reporte_avistado(models.Model):
             blank=True
         )  # image
 
-    id_reporte = models.ForeignKey(Reporte, default=None, verbose_name="reporte", on_delete=models.SET_DEFAULT)  # Foreign Key
+    id_reporte = models.ForeignKey(
+            Reporte,
+            default=None,
+            verbose_name="reporte",
+            on_delete=models.SET_DEFAULT
+        )  # Foreign Key
 
     class Meta:
         verbose_name_plural = "reporte_avistado"
@@ -174,7 +200,12 @@ class Reporte_encontrado(models.Model):
 
     mascota_recojida = models.BooleanField(default=False)  # boolean
 
-    id_reporte = models.ForeignKey(Reporte, default=None, verbose_name="reporte", on_delete=models.SET_DEFAULT)  # Foreign Key
+    id_reporte = models.ForeignKey(
+            Reporte,
+            default=None,
+            verbose_name="reporte",
+            on_delete=models.SET_DEFAULT
+        )  # Foreign Key
 
     class Meta:
         verbose_name_plural = "reporte_encontrado"
@@ -192,7 +223,12 @@ class Reporte_perdido(models.Model):
 
     ultimo_lugar_visto = models.CharField(max_length=100)  # String
 
-    id_reporte = models.ForeignKey(Reporte, default=None, verbose_name="reporte", on_delete=models.SET_DEFAULT)  # Foreign Key
+    id_reporte = models.ForeignKey(
+            Reporte,
+            default=None,
+            verbose_name="reporte",
+            on_delete=models.SET_DEFAULT
+        )  # Foreign Key
 
     class Meta:
         verbose_name_plural = "reporte_perdido"
