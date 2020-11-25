2361b1b1c90d69df3b914c1b14a5d2dc3800b787
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-24 23:17:28 -0600

    Update requirements and DATABASE

--- dogofinder/settings.py
+++ dogofinder/settings.py
@@ -94,18 +94,18 @@ WSGI_APPLICATION = 'dogofinder.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 
-    #    default': {
-    #        'ENGINE': 'django.db.backends.postgresql',
-    #        'NAME': os.getenv('NAME'),
-    #        'USER': os.getenv('USER'),
-    #        'PASSWORD': os.getenv('PASSWORD'),
-    #        'HOST': os.getenv('HOST')
-    #    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST')
+    }
 }
 
 
