58a9febf74aa1d2fbbc02fbc6ae74c9c56b927e8
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-25 00:13:32 -0600

    fix: Comment serializers

--- api/admin.py
+++ api/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Mascota)
-admin.site.register(Usuario)
-# admin.site.register(UsuarioRegistro)  # Nuevo
-admin.site.register(Mascota_perdida)
-admin.site.register(Mascota_encontrada)
-admin.site.register(Reporte)
-admin.site.register(Reporte_avistado)
-admin.site.register(Reporte_perdido)
-admin.site.register(Reporte_encontrado)
+# admin.site.register(Mascota)
+# admin.site.register(Usuario)
+# # admin.site.register(UsuarioRegistro)  # Nuevo
+# admin.site.register(Mascota_perdida)
+# admin.site.register(Mascota_encontrada)
+# admin.site.register(Reporte)
+# admin.site.register(Reporte_avistado)
+# admin.site.register(Reporte_perdido)
+# admin.site.register(Reporte_encontrado)
--- api/serializers.py
+++ api/serializers.py
@@ -1,39 +1,39 @@
 from rest_framework import serializers
-from .models import *
+# from .models import *
 
 
 ''' --------| Serializador para Mascota. |-------- '''
 
 
 # Serializador para clase Mascota.
-class Mascota_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mascota
-        fields = '__all__'
+# class Mascota_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Mascota
+#         fields = '__all__'
 
 
-# Serializador para clase Mascota_perdida.
-class Mascota_perdida_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mascota_perdida
-        fields = '__all__'
+# # Serializador para clase Mascota_perdida.
+# class Mascota_perdida_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Mascota_perdida
+#         fields = '__all__'
 
 
-# Serializador para clase Mascota_encontrada.
-class Mascota_encontrada_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Mascota_encontrada
-        fields = '__all__'
+# # Serializador para clase Mascota_encontrada.
+# class Mascota_encontrada_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Mascota_encontrada
+#         fields = '__all__'
 
 
-''' --------| Serializador para Usuario. |-------- '''
+# ''' --------| Serializador para Usuario. |-------- '''
 
 
-# Serializador para clase Usuario.
-class Usuario_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = '__all__'
+# # Serializador para clase Usuario.
+# class Usuario_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Usuario
+#         fields = '__all__'
 
 
 # Serializador para registro de usuario
@@ -71,28 +71,28 @@ class Usuario_serializer(serializers.ModelSerializer):
 
 
 # Serializador para clase Reporte.
-class Reporte_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reporte
-        fields = '__all__'
+# class Reporte_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Reporte
+#         fields = '__all__'
 
 
-# Serializador para clase Reporte_avistado.
-class Reporte_avistado_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reporte
-        fields = '__all__'
+# # Serializador para clase Reporte_avistado.
+# class Reporte_avistado_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Reporte
+#         fields = '__all__'
 
 
-# Serializador para clase Reporte_encontrado.
-class Reporte_encontrado_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reporte
-        fields = '__all__'
+# # Serializador para clase Reporte_encontrado.
+# class Reporte_encontrado_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Reporte
+#         fields = '__all__'
 
 
-# Serializador para clase Reporte_perdido.
-class Reporte_perdido_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reporte
-        fields = '__all__'
+# # Serializador para clase Reporte_perdido.
+# class Reporte_perdido_serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Reporte
+#         fields = '__all__'
